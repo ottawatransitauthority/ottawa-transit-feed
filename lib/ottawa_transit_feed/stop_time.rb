e29874356d15d3b08f8b3f973ebc9f73f02f6e35
Craig Davey <me@craigdavey.ca>
2010-03-21 04:30:57 -0400

    Marked required attributes in StopTime.

--- lib/ottawa_transit_feed/stop_time.rb
+++ lib/ottawa_transit_feed/stop_time.rb
@@ -5,6 +5,8 @@ module OttawaTransitFeed
 
     feed_attributes :trip_id, :stop_id, :stop_sequence, :arrival_time, :departure_time, :pickup_type, :drop_off_type
     
+    validates_presence_of :trip_id, :stop_id, :stop_sequence, :arrival_time, :departure_time
+
     belongs_to :stop, :primary_key => :stop_id
     belongs_to :trip, :primary_key => :trip_id
     
