02ecd04ddfea9ed2aa80a87db03ec2c682686b81
Craig Davey <me@craigdavey.ca>
2010-04-03 04:12:52 -0400

    Added basic calendar import test.

--- test/ottawa_transit_feed/calendar_test.rb
+++ test/ottawa_transit_feed/calendar_test.rb
@@ -2,5 +2,15 @@ require "#{File.dirname(File.dirname(__FILE__))}/test_helper"
 
 module OttawaTransitFeed
   class CalendarTest < ActiveSupport::TestCase
+    
+    def setup
+      OttawaTransitFeed.clear_database
+    end
+    
+    test "4 calendars should be imported from fixtures" do 
+      assert_difference "Calendar.count", +4 do
+        Calendar.import "test/fixtures/transit_feeds/oc_transpo"
+      end
+    end
   end
 end
