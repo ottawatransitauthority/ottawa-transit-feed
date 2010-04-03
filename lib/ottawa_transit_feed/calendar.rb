6576e32b381746efb68da1595a4472863cef3eb2
Craig Davey <me@craigdavey.ca>
2010-04-03 16:47:14 -0400

    Comments on calendars and calendar dates.

--- lib/ottawa_transit_feed/calendar.rb
+++ lib/ottawa_transit_feed/calendar.rb
@@ -1,6 +1,14 @@
 module OttawaTransitFeed
   class Calendar < Record
 
+    # Calendar records contain a service_id that uniquely identifies a set of dates when transit 
+    # service is available for one or more routes. 
+    #
+    # The service_id value is dataset unique. It is referenced by trips and calendar dates.
+    # 
+    # The calendar dataset provided in the OC Transpo feed is usually accurate and complete. 
+    # Ottawa Trasnsit Feed doesn’t apply any changes to this dataset.
+
     extend Feeder
 
     feed_attributes :service_id, :start_date, :end_date, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday
--- lib/ottawa_transit_feed/calendar_date.rb
+++ lib/ottawa_transit_feed/calendar_date.rb
@@ -1,5 +1,25 @@
 module OttawaTransitFeed
   class CalendarDate < Record
+    
+    # Calendar date records allow you to explicitly activate or disable calendar services by date. 
+    #
+    # The General Transit Specification defines two ways to use them:
+    #   
+    #   Recommended: 
+    #   Use calendar_dates.txt in conjunction with calendar.txt, where calendar_dates.txt defines
+    #   any exceptions to the default service categories defined in the calendar.txt file. If your 
+    #   service is generally regular, with a few changes on explicit dates (for example, to 
+    #   accomodate special event services, or a school schedule), this is a good approach.
+    #   
+    #   Alternate: 
+    #   Omit calendar.txt, and include ALL dates of service in calendar_dates.txt. If your schedule
+    #   varies most days of the month, or you want to programmatically output service dates without 
+    #   specifying a normal weekly schedule, this approach may be preferable.
+    #
+    # Ottawa Transit Feed follows the recommended practice. 
+    #
+    # The calendar date dataset provided in the OC Transpo feed is usually accurate and complete. 
+    # Ottawa Trasnsit Feed doesn’t apply any changes to this dataset.
 
     extend Feeder
 
