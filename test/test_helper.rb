0f37d5a7f2068c387df869ab91e99f0a2e50e6ea
Craig Davey <me@craigdavey.ca>
2010-04-03 01:17:26 -0400

    Updated test helper.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,3 +7,14 @@ require "active_support/test_case"
 
 OttawaTransitFeed.env = :test
 OttawaTransitFeed.reset_database
+
+class Date
+  
+  # date.at('12:00') => time object representing the date at noon
+  def at (string)
+    matches  = /^(\d\d)(:)*(\d\d)$/.match(string)
+    hh, mm   = Integer(matches[1]), Integer(matches[3])
+    duration = hh.hours + mm.minutes
+    to_time.in_time_zone.change :hour => hh, :min => mm
+  end
+end
