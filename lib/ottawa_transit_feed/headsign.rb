ba8c5b59ccd77bf66e0f22df0708f6f7c1845e0a
Craig Davey <me@craigdavey.ca>
2010-04-03 23:48:53 -0400

    Fleshed out headsign domain.

--- lib/ottawa_transit_feed/headsign.rb
+++ lib/ottawa_transit_feed/headsign.rb
@@ -8,12 +8,46 @@ module OttawaTransitFeed
     
     belongs_to :route, :foreign_key => :route_id, :primary_key => :route_id
     
+    has_many :trips, :foreign_key => :headsign_signature, :primary_key => :signature
+    
     before_validation :set_signature
     
     serialize :stop_ids
 
+    named_scope :all_missing_strings, :conditions => {:string => nil}
+
+    def stops
+      stop_ids.map do |stop_id|
+        Stop.find_by_stop_id stop_id
+      end
+    end
+    
+    def message
+      /\d+ (.*)/.match(string)[1]
+    end
+    
     def set_signature
       self.signature = Headsign.signature_for route, stop_ids
+    end
+    
+    def set_string
+      if message = find_message
+        self.string = "#{route.number} #{message}"
+      end
+    end
+    
+    def find_message
+      trips.each do |trip|
+        if message = find_message_by_trip(trip)
+          return message
+        end
+      end
+    end
+    
+    def find_message_by_trip (trip)
+      if departure = trip.find_remote_departure
+        return departure[:destination]
+      end
     end
   end
 end
