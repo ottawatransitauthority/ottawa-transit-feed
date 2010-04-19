7cd6a30c3bb3a25c7cf4b5c1c65c936f69074b67
rpeden <ryan04@gmail.com>
2010-04-19 17:07:16 +0800

    disabled some SQLite safety features that aren't useful for this application's use of the database. Also increased the DB cache size so that the DB be mostly held in memory while processing it. Seems to have provided a very large boost in gtfs data import speed.

--- lib/ottawa_transit_feed/record.rb
+++ lib/ottawa_transit_feed/record.rb
@@ -2,7 +2,13 @@ module OttawaTransitFeed
   class Record < ActiveRecord::Base
     
     establish_connection :adapter => 'sqlite3', :database => "db/#{OttawaTransitFeed.env}.sqlite3", :timeout => 120000
-    
+
+    self.connection.execute("PRAGMA default_cache_size = 200000")
+    self.connection.execute("PRAGMA default_synchronous = OFF")
+    self.connection.execute("PRAGMA journal_mode = OFF")
+    self.connection.execute("PRAGMA temp_store = MEMORY")
+
+
     self.abstract_class = true
     
     self.logger = Logger.new("log/#{OttawaTransitFeed.env}.log")
